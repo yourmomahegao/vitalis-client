01bb66fc4cc5ddef61bb76895654c2b63e08ed0a
yourmomahegao <mrdumbs70@gmail.com>
2026-08-05 16:08:56 +0300

    feat: integrate better-sqlite3 for database management and implement encryption methods

--- src/js/lib.js
+++ src/js/lib.js
@@ -1,3 +1,6 @@
 const path = require("path");
 const $ = require("jquery");
 const { ipcRenderer } = require('electron')
+const Database = require("better-sqlite3");
+const { app } = require("@electron/remote");
+const crypto = require("crypto");
